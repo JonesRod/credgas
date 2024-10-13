6e7e422dd6bdb32ffe7bdb0d4489eb0879246886
JonesRod <batatajonesrodrigues@gmail.com>
2024-10-13 00:16:35 -0400

    Update perfil_loja.php

--- login/lib/paginas/parceiros/perfil_loja.php
+++ login/lib/paginas/parceiros/perfil_loja.php
@@ -44,6 +44,7 @@
         $logo = '../arquivos_fixos/icone_loja.jpg';
     }
 
+
     // Consulta para buscar produtos do catálogo
     $produtos_catalogo = $mysqli->query(query: "SELECT * FROM produtos WHERE id_loja = '$id'") or die($mysqli->error);
 
@@ -51,7 +52,7 @@
     $promocoes = $mysqli->query(query: "SELECT * FROM produtos WHERE promocao = 1 AND id_loja = '$id'");
     $mais_vendidos = $mysqli->query(query: "SELECT * FROM produtos WHERE mais_vendidos = 1 AND id_loja = '$id'");
     $frete_gratis = $mysqli->query(query: "SELECT * FROM produtos WHERE frete_gratis = 1 AND id_loja = '$id'");
-?>
+?>      
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -73,91 +74,103 @@
         <span id="msgAlerta"></span><br>
 
         <label for="razao">Razão Social:</label>
-        <input type="text" id="razao" name="razao" required>
+        <input type="text" id="razao" name="razao" required value="<?php echo $dados['razao']?>">
 
         <label for="nomeFantasia">Nome Fantasia:</label>
-        <input type="text" id="nomeFantasia" name="nomeFantasia" required>
+        <input type="text" id="nomeFantasia" name="nomeFantasia" required value="<?php echo $dados['nomeFantasia']?>">
 
         <label for="cnpj">CNPJ:</label>
-        <input type="text" id="cnpj" name="cnpj" required oninput="formatCNPJ(this)" onblur="verificaCNPJ()">
+        <input type="text" id="cnpj" name="cnpj" required value="<?php echo $dados['cnpj']?>" oninput="formatCNPJ(this)" onblur="verificaCNPJ()">
 
         <label for="inscricaoEstadual">Inscrição Estadual:</label>
-        <input type="text" id="inscricaoEstadual" name="inscricaoEstadual" required oninput="this.value = this.value.replace(/\D/g, '')">
+        <input type="text" id="inscricaoEstadual" name="inscricaoEstadual" required value="<?php echo $dados['inscricaoEstadual'];?>" oninput="this.value = this.value.replace(/\D/g, '')">
 
         <label for="categoria">Categoria:</label>
-        <input type="text" id="categoria" name="categoria" required>
+        <input type="text" id="categoria" name="categoria" required value="<?php echo $dados['categoria']?>">
 
         <!-- Div para visualização da imagem ou do nome do arquivo -->
         <div id="filePreview" class="file-preview"></div>
         <div class="file-upload-container">
             <label for="arquivoEmpresa">Comprovante de Inscrição e de Situação Cadastral (PDF ou PNG):</label>
-            <input type="file" id="arquivoEmpresa" name="arquivoEmpresa" accept=".pdf, .png" required>
+            <img src="arquivos/<?php echo $dados['anexo_comprovante'];?>" alt="" style="max-width: 400px;">
+            <input type="file" id="arquivoEmpresa" name="arquivoEmpresa" accept=".pdf, .png" required value="<?php echo $dados['anexo_comprovante']?>">
         </div>
 
         <label for="telefoneComercial">Telefone Comercial:(WhatsApp)</label>
-        <input type="text" id="telefoneComercial" name="telefoneComercial" required placeholder="(00) 00000-0000" oninput="formatarCelular(this)" onblur="verificaCelular1()">
+        <input type="text" id="telefoneComercial" name="telefoneComercial" required value="<?php echo $dados['telefoneComercial']?>" placeholder="(00) 00000-0000" oninput="formatarCelular(this)" onblur="verificaCelular1()">
 
         <label for="telefoneResponsavel">Telefone do Responsável:(WhatsApp)</label>
-        <input type="text" id="telefoneResponsavel" name="telefoneResponsavel" required placeholder="(00) 00000-0000" oninput="formatarCelular(this)" onblur="verificaCelular2()">
+        <input type="text" id="telefoneResponsavel" name="telefoneResponsavel" required value="<?php echo $dados['telefoneResponsavel']?>" placeholder="(00) 00000-0000" oninput="formatarCelular(this)" onblur="verificaCelular2()">
         
         <label for="email">E-mail:</label>
-        <input required name="email" id="email" type="email" required>
-
-        <label for="senha">Senha:</label>
-        <input required name="senha" id="senha" placeholder="Mínimo 6 dígitos" type="password">
+        <input required name="email" id="email" type="email" required value="<?php echo $dados['email']?>" >
 
         <p id="status-localizacao">Localização: Ativa</p>
 
         <label for="cep">CEP:</label>
-        <input required name="cep" id="cep" type="text" maxlength="9" oninput="formatarCEP(this)" onblur="buscarCidadeUF()">
+        <input required name="cep" id="cep" type="text" value="<?php echo $dados['cep']?>" maxlength="9" oninput="formatarCEP(this)" onblur="buscarCidadeUF()">
 
         <label for="uf">Estado: </label>
         <select required name="uf" id="uf">
-            <option value="Escolha">---Escolha---</option>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
+            <?php
+                $estados = array(
+                'AC' => 'Acre',
+                'AL' => 'Alagoas',
+                'AP' => 'Amapá',
+                'AM' => 'Amazonas',
+                'BA' => 'Bahia',
+                'CE' => 'Ceará' ,
+                'DF' => 'Distrito Federal',
+                'ES' => 'Espírito Santo',
+                'GO' => 'Goiás',
+                'MA' => 'Maranhão',
+                'MS' => 'Mato Grosso do Sul',
+                'MT' => 'Mato Grosso',
+                'MG' => 'Minas Gerais',
+                'PA' => 'Pará',
+                'PB' => 'Paraíba',
+                'PR' => 'Paraná',
+                'PE' => 'Pernambuco',
+                'PI' => 'Piauí',
+                'RJ' => 'Rio de Janeiro',
+                'RN' => 'Rio Grande do Norte',
+                'RS' => 'Rio Grande do Sul',
+                'RO' => 'Rondônia',
+                'RR' => 'Roraima',
+                'SC' => 'Santa Catarina',
+                'SP' => 'São Paulo',
+                'SE' => 'Sergipe',
+                'TO' => 'Tocantins'
+                );
+
+                $ufSelecionada = $dados['estado'];
+
+                echo '<option value="' . $ufSelecionada . '">' . $estados[$ufSelecionada] . '</option>';
+
+                foreach ($estados as $uf => $estado) {
+                    if ($uf !== $ufSelecionada) {
+                        echo '<option value="' . $uf . '">' . $estado . '</option>';
+                    }
+                    }           
+        ?>
+
+        <!-- Adicione mais opções para outros estados aqui -->
         </select>
 
         <label for="cidade">Cidade:</label>
-        <input type="text" id="cidade" name="cidade" required>
+        <input type="text" id="cidade" name="cidade" required value="<?php echo $dados['cidade']?>">
 
         <label for="rua">RUA/AV:</label>
-        <input type="text" id="rua" name="rua" required>
+        <input type="text" id="rua" name="rua" required value="<?php echo $dados['endereco']?>" >
 
         <label for="numero">Numero:</label>
-        <input type="text" id="numero" name="numero" required>
+        <input type="text" id="numero" name="numero" required value="<?php echo $dados['numero']?>">
 
         <label for="bairro">Bairro:</label>
-        <input type="text" id="bairro" name="bairro" required>
+        <input type="text" id="bairro" name="bairro" required value="<?php echo $dados['bairro']?>">
 
-        <div class="checkbox-container">
-            <input type="checkbox" id="aceito" name="aceito" value="sim" required onchange="verificarAceite()">Eu aceito os <a href="termos.php" target="_blank"><b>Termos</b></a>.
-        </div>
+        <a href="termos.php" target="_blank"><b>Termos</b></a>.
+
         <div class="action-buttons">
             <a href="parceiro_home.php" class="link-voltar"><b>Voltar</b></a>
             <button type="submit" id="cadastrar" disabled>Cadastrar</button>
